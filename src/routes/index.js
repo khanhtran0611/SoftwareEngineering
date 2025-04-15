7df41f2e960ec380fe3d7a0663e39321684dae08
khanhtran0611 <163338296+khanhtran0611@users.noreply.github.com>
2025-04-15 16:27:21 +0700

    Delete unnecessaries

--- src/routes/index.js
+++
@@ -1,8 +0,0 @@
-const afterLogged = require('./AfterLogged.js')
-const login = require('./Logging.js')
-function route(app){
-    app.use('/:user_id',afterLogged)
-    app.use('/',login)
-}
-
-module.exports = route;
